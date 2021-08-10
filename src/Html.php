166d9958a6b97bccb97336f86c75f5cba564ee7d
Arne Blankerts <Arne@Blankerts.de>
2021-08-10 10:32:04 +0200

    Introduce fluent interface for HTML

--- src/Html.php
+++ src/Html.php
@@ -14,7 +14,10 @@ class Html {
     }
 
     public function applySnippets(SnippetListCollection $snippetListCollection): void {
+    public function applySnippets(SnippetListCollection $snippetListCollection): self {
         (new SnippetRenderer($snippetListCollection))->render($this->dom->documentElement);
+
+        return $this;
     }
 
     /**
@@ -22,23 +25,32 @@ class Html {
      *
      * @throws ViewModelRendererException
      */
-    public function applyViewModel($model): void {
+    public function applyViewModel($model): self {
         (new ViewModelRenderer())->render($this->dom->documentElement, $model);
+
+        return $this;
     }
 
     /**
      * @throws FormDataRendererException
      */
-    public function applyFormData(FormData $formData): void {
+    public function applyFormData(FormData $formData): self {
         (new FormDataRenderer())->render($this->dom->documentElement, $formData);
+
+        return $this;
     }
 
-    public function applyCSRFProtection(CSRFProtection $protection): void {
+    public function applyCSRFProtection(CSRFProtection $protection): self {
         (new CSRFProtectionRenderer())->render($this->dom->documentElement, $protection);
+
+        return $this;
     }
 
-    public function applyTransformation(Transformation $transformation): void {
+    public function applyTransformation(Transformation $transformation): self {
         (new TransformationProcessor())->process($this->dom->documentElement, $transformation);
+
+        return $this;
+    }
     }
 
     public function asString(Filter $filter = null): string {
