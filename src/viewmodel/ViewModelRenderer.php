89903d393d2c8384793a6a20407d47d9c7e1a316
Arne Blankerts <Arne@Blankerts.de>
2023-07-28 17:26:38 +0200

    Refactor: Avoid break

--- src/viewmodel/ViewModelRenderer.php
+++ src/viewmodel/ViewModelRenderer.php
@@ -611,10 +611,7 @@ final class ViewModelRenderer {
     private function getModelPath(DOMElement $context): string {
         $list = [$context->getAttribute('property')];
 
-        while ($context = $context->parentNode) {
-            if ($context instanceof DOMDocument) {
-                break;
-            }
+        while ($context = $context->parentNode and !$context instanceof DOMDocument) {
             assert($context instanceof DOMElement);
 
             if (!$context->hasAttribute('property')) {
