0c736c9289903a62d045375e1e7402e98e410175
Arne Blankerts <Arne@Blankerts.de>
2023-11-24 16:32:09 +0100

    Alternative implementation using XMLWriter

--- src/transformation/NamespaceCleaningTransformation.php
+++ src/transformation/NamespaceCleaningTransformation.php
@@ -9,6 +9,8 @@
  */
 namespace Templado\Engine;
 
+use DOMDocumentFragment;
+use XMLWriter;
 use function assert;
 use DOMAttr;
 use DOMDocument;
@@ -18,54 +20,98 @@ use DOMNode;
 class NamespaceCleaningTransformation implements Transformation {
     private const HTMLNS = 'http://www.w3.org/1999/xhtml';
 
+    private bool $isFirst;
+
     public function selector(): Selector {
-        return new XPathSelector('//*');
+        return new XPathSelector('.');
     }
 
     public function apply(DOMNode $context): void {
         assert($context instanceof DOMElement);
+        assert($context->parentNode instanceof DOMNode);
 
-        if ($this->hasEmptyNamespace($context) ||
-            $this->isPrefixedHTML($context) ||
-            $this->isChildWithHtmlXMLNS($context)
-        ) {
-            $this->enforceProperNamespace($context);
-        }
+        $this->isFirst = true;
+
+        $context->parentNode->replaceChild(
+            $this->cleanup($context),
+            $context
+        );
     }
 
-    private function enforceProperNamespace(DOMElement $context): void {
-        assert($context->ownerDocument instanceof DOMDocument);
+    private function cleanup(DOMElement $context): DOMNode {
+        $writer = new XMLWriter();
+        $writer->openMemory();
+        $writer->startDocument();
 
-        $replacement = $context->ownerDocument->createElementNS(
-            self::HTMLNS,
-            $context->localName
+        $this->walk($writer, $context, []);
+
+        $writer->endDocument();
+
+        /*
+        assert($context->ownerDocument instanceof DOMDocument);
+        $tmpContainer = $context->ownerDocument->createDocumentFragment();
+
+        var_dump($writer->outputMemory());
+        die;
+
+        $tmpContainer->appendXML($writer->outputMemory());
+*/
+        $dom = new DOMDocument();
+        $dom->loadXML($writer->outputMemory());
+
+        assert($context->ownerDocument instanceof DOMDocument);
+        return $context->ownerDocument->importNode(
+            $dom->documentElement, true
         );
 
-        if ($context->hasAttributes()) {
-            foreach (StaticNodeList::fromNamedNodeMap($context->attributes) as $attribute) {
-                assert($attribute instanceof DOMAttr);
+    }
 
-                $replacement->setAttributeNodeNS($attribute);
+    private function walk(XMLWriter $writer, DOMNode $node, array $knownPrefixes):void {
+        assert($node->ownerDocument instanceof DOMDocument);
+
+        if (!$node instanceof DOMElement) {
+            $writer->writeRaw(
+                $node->ownerDocument->saveXML($node)
+            );
+
+            return;
+        }
+
+        if ($node->namespaceURI === self::HTMLNS || empty($node->namespaceURI)) {
+            $writer->startElement($node->localName);
+            if ($this->isFirst) {
+                $writer->writeAttribute('xmlns', self::HTMLNS);
+                $this->isFirst = false;
+            }
+        } else {
+            if (empty($node->prefix)) {
+                $writer->startElement($node->nodeName);
+                $writer->writeAttribute('xmlns', $node->namespaceURI);
+            } else {
+                $writer->startElementNs($node->prefix, $node->localName, $node->namespaceURI);
             }
         }
 
-        if ($context->hasChildNodes()) {
-            $replacement->append(...$context->childNodes);
+        foreach($node->attributes as $attribute) {
+            assert($attribute instanceof DOMAttr);
+
+            if (empty($attribute->prefix)) {
+                $writer->writeAttribute($attribute->name, $attribute->value);
+                continue;
+            }
+
+            if (!isset($knownPrefixes[$attribute->prefix])) {
+                $knownPrefixes[$attribute->prefix] = $node->lookupNamespaceURI($attribute->prefix);
+                $writer->writeAttribute('xmlns:' . $attribute->prefix, $node->lookupNamespaceURI($attribute->prefix));
+            }
         }
 
-        assert($context->parentNode instanceof DOMNode);
-        $context->parentNode->replaceChild($replacement, $context);
-    }
+        if ($node->hasChildNodes()) {
+            foreach($node->childNodes as $childNode) {
+                $this->walk($writer, $childNode, $knownPrefixes);
+            }
+        }
 
-    private function hasEmptyNamespace(DOMElement $context): bool {
-        return $context->namespaceURI === '' || $context->namespaceURI === null;
-    }
-
-    private function isPrefixedHTML(DOMElement $context): bool {
-        return $context->namespaceURI === self::HTMLNS && $context->prefix !== '';
-    }
-
-    private function isChildWithHtmlXMLNS(DOMElement $context): bool {
-        return empty($context->prefix) && ($context->getAttribute('xmlns') === self::HTMLNS);
+        $writer->endElement();
     }
 }
